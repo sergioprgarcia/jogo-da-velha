c69d21def932911778cedc9218bdd2b8d0ce4dfa
sergioprgarcia <serjinn@gmail.com>
2020-05-15 15:16:12 -0300

    #4 Adicionado jogadores e mais regras para o tabuleiro.

--- jogo-da-velha/src/main/java/br/com/slpgarcia/jogodavelha/principal/Jogo.java
+++ jogo-da-velha/src/main/java/br/com/slpgarcia/jogodavelha/principal/Jogo.java
@@ -4,16 +4,28 @@ import br.com.slpgarcia.jogodavelha.Constants;
 import br.com.slpgarcia.jogodavelha.ui.UI;
 
 public class Jogo {
-	
+
 	private Tabuleiro tabuleiro = new Tabuleiro();
 	private Jogador[] jogadores = new Jogador[Constants.SIMBOLO_JOGADORES.length];
-	
+
 	public void jogar() {
-		
+
 		UI.imprirTituloJogo();
-		
-		tabuleiro.imprimir();
-		
+
+		for(int i = 0; i < jogadores.length; i++) {
+			jogadores[i] = criarJogador(i);
+		}
+
 	}
+
+	private Jogador criarJogador(int index) {
+		String nome = UI.entradaDados("Jogador " + (index + 1) + " =>");
+		char simbolo = Constants.SIMBOLO_JOGADORES[index];
+		Jogador jogador = new Jogador(nome, tabuleiro, simbolo);
+
+		UI.imprimirTexto("O jogador '" + nome + "' vai usar o símbolo '" + simbolo + "'");
+		return jogador;
+	}
+
 
 }
--- jogo-da-velha/src/main/java/br/com/slpgarcia/jogodavelha/principal/Tabuleiro.java
+++ jogo-da-velha/src/main/java/br/com/slpgarcia/jogodavelha/principal/Tabuleiro.java
@@ -6,13 +6,13 @@ import br.com.slpgarcia.jogodavelha.ui.UI;
 public class Tabuleiro {
 
 	private char [][] matriz;
-	
+
 	public Tabuleiro() {
 		matriz = new char [Constants.TAMANHO_TABULEIRO] [Constants.TAMANHO_TABULEIRO];
 		limpar();
-		
+
 	}
-	
+
 	public void limpar() {
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
@@ -20,21 +20,21 @@ public class Tabuleiro {
 			}
 		}
 	} 
-	
+
 	public void imprimir() {
 		UI.imprimirNovaLinha();
-		
+
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				UI.imprimirTextoSemNovaLinha(String.valueOf(matriz[i][j]));
-				
+
 				if(j < matriz[i].length - 1 ){
 					UI.imprimirTextoSemNovaLinha("  |");
 				}
 			}
 
 			UI.imprimirNovaLinha();
-			
+
 			if( i < matriz[i].length -1 ) {
 				UI.imprimirTexto("-----------");
 			}
@@ -49,18 +49,89 @@ public class Tabuleiro {
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	public boolean jogar(Jogador jogador, Movimentar movimentar) {
 		int i = movimentar.getI();
 		int j = movimentar.getJ();
-		
+
+		//TODO valimentar os movimentos do jogador
+
 		matriz[i][j] = jogador.getSimbolo();
 		
-		//TODO Checar se o jogador ganhou;
-		return false;
 		
+		return checarLinhas(jogador) || checarColunas(jogador) || checarDiagonalDir(jogador) || checarDiagonalEsq(jogador);
 	}
+
+	private boolean checarLinhas(Jogador jogador) {
+		for(int i = 0; i < Constants.TAMANHO_TABULEIRO; i++) {
+			if (checarLinha(i, jogador)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean checarLinha(int i, Jogador jogador) {
+		char simbolo = jogador.getSimbolo();
+
+
+		for(int j = 0; j < Constants.TAMANHO_TABULEIRO; j++) {
+			if(matriz[i][j] != simbolo) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private boolean checarColunas(Jogador jogador) {
+		for(int j = 0; j < Constants.TAMANHO_TABULEIRO; j++) {
+			if (checarLinha(j, jogador)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean checarColuna(int j, Jogador jogador) {
+		char simbolo = jogador.getSimbolo();
+
+
+		for(int i = 0; i < Constants.TAMANHO_TABULEIRO; i++) {
+			if(matriz[i][j] != simbolo) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private boolean checarDiagonalEsq(Jogador jogador) {
+		char simbolo = jogador.getSimbolo();
+
+		for(int i = 0; i < Constants.TAMANHO_TABULEIRO; i++) {
+			if(matriz[i][i] != simbolo) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean checarDiagonalDir(Jogador jogador) {
+		char simbolo = jogador.getSimbolo();
+		int ultimaLinha = Constants.TAMANHO_TABULEIRO -1;
+
+		for(int i = ultimaLinha, j = 0; i >= 0; i--, j++) {
+			if(matriz[i][j] != simbolo) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
